ea0fc12de21c7f6b443b31dec96dcec24a7ff0e9
starrising321 <74265690+starrising321@users.noreply.github.com>
2022-02-26 03:02:18 +0500

    Add files via upload

---
+++ kebu/about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#     Premium Auto Script By Geo•NTB            #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"  | lolcat
+echo -e "# For VPS with KVM and VMWare virtualization    #"  | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Thanks To  :                                  #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# ALLAH S.W.T                                   #"  | lolcat
+echo -e "# My Family                                     #"  | lolcat
+echo -e "# Google                                        #"  | lolcat
+echo -e "#                                               #"  | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Created By : GEO GABUT                        #"  | lolcat
+echo -e "# Contact    : t.me/kangiciper                  #"  | lolcat
+echo -e "# Whatsapp   : 082339191527                     #"  | lolcat
+echo -e "=================================================" | lolcat
